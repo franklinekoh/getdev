eb4a856fdc07cdadd3dbe678f66523e3c8b3eef0
fekoh <mbj/4592>
2019-12-04 11:13:40 +0100

    modified the server listener

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const loadApp = require('./app');
 function startServer() {
     const app = loadApp.app;
 
-    let port = process.env.APP_PORT || 5000;
+    let port = process.env.PORT || 5000;
     app.listen(port, ()=>{
         console.log(`Server listening on port: ${port}`);
     });
